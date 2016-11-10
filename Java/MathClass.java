839658712036c695aee74a2f5805c1717966fa8e
natasyaav <natasyavirgichalia@natasyas-MacBook-Pro.local>
2016-11-09 23:08:34 -0500

    add 5.1 assignment part 2

---
+++ Java/MathClass.java
@@ -0,0 +1,24 @@
+public class MathClass {
+	public static void main (String[] args) {
+		//Power
+		System.out.println("5 to the power of 2 is " + Math.pow(5,2));
+		//square root
+		System.out.println("the square root of 25 is " + Math.sqrt(25));
+		//absolute
+		System.out.println("the absolute of -100 is " + Math.abs(-100));
+		//round a number to the closest 
+		System.out.println("6.789 rounds to " + Math.round(6.789));
+		//round a number up
+		System.out.println("8.5 rounds up to " + Math.ceil(8.5));
+		//round a number down
+		System.out.println("8.43 rounds down to " + Math.floor(8.43));
+		//random
+		System.out.println("a random number between 0 and 1 is " + Math.random());
+		//maximum
+		System.out.println("between 75 and 90 the max is " + Math.max(75, 90));
+		//minimum
+		System.out.println("between -99 and 99 the min is " +Math.min(-99, 99));
+		}
+	
+}
+
