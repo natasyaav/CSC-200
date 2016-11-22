a01c3bdf4d0b6937c004983c182510856ecdf15b
natasyaav <natasyavirgichalia@natasyas-MacBook-Pro.local>
2016-11-22 00:27:12 -0500

    add 5.4 new Car Class

--- Desktop/CSC200/5.3InstanceVariable/src/Car.java
+++ Desktop/CSC200/5.3InstanceVariable/src/Car.java
@@ -1,97 +1,51 @@
-/**
- * @author Natasya Virgichalia <br>
- * date: 11/17/2016 <br>
- * contact: nvirgichalia@gmail.com <br>
- * Car class composes of the car's color, horse power, engine size, and brand <br>
- */
+
 public class Car {
 	private String Color;
 	private int HorsePower;
 	private double EngineSize;
 	private String Brand;
+	private static int count=0;
 	
-	/**
-	 * construct a new Car with color = white, horse power = 500, engine size = 4.5, brand = BMW.
-	 * method that initialize the value of the attributes.
-	 */
 	public Car() {
-		Color = "White";
-		HorsePower = 500;
-		EngineSize = 4.5;
-		Brand = "BMW";
+		this("White", 500, 4.5, "BMW");
 	}
-	/**
-	 * construct a new with the values provided from color, horse power, engine size, and brand  parameters.
-	 * @param Color of the car
-	 * @param HorsePower of the car
-	 * @param EngineSize of the car
-	 * @param Brand of the car
-	 */
 	public Car(String Color, int HorsePower, double EngineSize, String Brand) {
 		this.Color = Color;
 		this.HorsePower = HorsePower;
 		this.EngineSize = EngineSize;
 		this.Brand = Brand;
+		count++;
 	}
-	/**
-	 * setter method to set the color of a car according to the color parameter.
-	 * @param Color
-	 */
 	public void setColor(String Color) {
 		this.Color = Color;
 	}
-	/**
-	 * setter method to set the horse power of a car according to the horse power parameter.
-	 * @param HorsePower
-	 */
 	public void setHorsePower(int HorsePower) {
 		this.HorsePower = HorsePower;
 	}
-	/**
-	 * setter method to set the engine size of a car according to the engine size parameter.
-	 * @param EngineSize
-	 */
 	public void setEngineSize(double EngineSize) {
 		this.EngineSize = EngineSize;
 	}
-	/**
-	 * setter method to set the brand of a car according to the brand parameter.
-	 * @param Brand
-	 */
 	public void setBrand(String Brand) {
 		this.Brand = Brand;
 	}
-	/**
-	 * getter method to get a car's color.
-	 * @return the color of a car.
-	 */
 	public String getColor() {
 		return Color;
 	}
-	/**
-	 * getter method to get a car's horse power.
-	 * @return the horse power of a car.
-	 */
 	public int getHorsePower() {
 		return HorsePower;
 	}
-	/**
-	 * getter method to get a car's engine size.
-	 * @return the engine size of a car.
-	 */
 	public double getEngineSize() {
 		return EngineSize;
 	}
-	/**
-	 * getter method to get a car's brand.
-	 * @return the brand of a car.
-	 */
 	public String getBrand() {
 		return Brand;
 	}
-	/**
-	 * return the color, horse power, engine size, and brand of a car.
-	 */
+	public static int getCount() {
+		return count;
+	}
+	public boolean equals(Car car) {
+		return this.Color.equals(car.Color) && this.HorsePower == car.HorsePower && this.EngineSize == car.EngineSize && this.Brand.equals(car.Brand);
+	}
 	public String toString() {
 		return  this.Brand + ", " + this.Color + ", " + this.HorsePower + ", " + this.EngineSize;
 	}
